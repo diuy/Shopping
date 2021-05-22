bf81370e0ef38a2de1e3ac93e7be8f5691976113
Tara <bb@126.com>
2021-05-22 20:41:30 +0800

    create notification

--- app/src/main/java/com/example/shopping/ShoppingService.java
+++ app/src/main/java/com/example/shopping/ShoppingService.java
@@ -2,11 +2,21 @@ package com.example.shopping;
 
 import android.accessibilityservice.AccessibilityService;
 import android.accessibilityservice.GestureDescription;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Path;
+import android.os.Build;
 import android.os.Handler;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
 
 import com.example.shopping.task.Task;
 import com.example.shopping.task.TaskFactory;
@@ -38,8 +48,11 @@ public class ShoppingService extends AccessibilityService {
     @Override
     public void onCreate() {
         super.onCreate();
-        toast("onCreate");
-
+//        toast("onCreate");
+        // 获取服务通知
+        Notification notification = createForegroundNotification();
+        //将服务置于启动状态 ,NOTIFICATION_ID指的是创建的通知的ID
+        startForeground(1, notification);
     }
 
     private void onSchedule(String name) {
@@ -213,6 +226,7 @@ public class ShoppingService extends AccessibilityService {
         toast("Shopping closed");
         service = null;
         schedule.stop();
+        stopForeground(true);
         super.onDestroy();
     }
 
@@ -224,5 +238,46 @@ public class ShoppingService extends AccessibilityService {
         return service != null;
     }
 
+    private Notification createForegroundNotification() {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
+        // 唯一的通知通道的id.
+        String notificationChannelId = "notification_channel_id_01";
+
+        // Android8.0以上的系统，新建消息通道
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            //用户可见的通道名称
+            String channelName = "Shopping Notification";
+            //通道的重要程度
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel notificationChannel = new NotificationChannel(notificationChannelId, channelName, importance);
+            notificationChannel.setDescription("Shopping Channel");
+            //LED灯
+//            notificationChannel.enableLights(true);
+//            notificationChannel.setLightColor(Color.RED);
+            //震动
+//            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+//            notificationChannel.enableVibration(true);
+            if (notificationManager != null) {
+                notificationManager.createNotificationChannel(notificationChannel);
+            }
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, notificationChannelId);
+        //通知小图标
+        builder.setSmallIcon(R.drawable.ic_launcher_foreground);
+        //通知标题
+        builder.setContentTitle("Shopping");
+        //通知内容
+        builder.setContentText("运行中");
+        //设定通知显示的时间
+        builder.setWhen(System.currentTimeMillis());
+        //设定启动的内容
+        Intent activityIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1, activityIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(pendingIntent);
+
+        //创建通知并返回
+        return builder.build();
+    }
 }
